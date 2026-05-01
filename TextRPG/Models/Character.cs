f10217e7fbd4c04f2bea60ddce329807d4426a34
doyoung930 <doyoung930@gmail.com>
2026-05-01 21:16:28 +0900

    player: Character 기본 추상 클래스

---
+++ TextRPG/Models/Character.cs
@@ -0,0 +1,38 @@
+﻿namespace TextRPG.Models;
+
+// 캐릭터 기본 추상 클래스
+public abstract class Character
+{
+    #region 프로퍼티
+    public string Name { get; protected set; }
+    public int CurrentHp { get; protected set; }
+    public int MaxHp { get; protected set; }
+    public int CurrentMp { get; protected set; }
+    public int MaxMp { get; protected set; }
+    public int AttackPower { get; protected set; }
+    public int Defence { get; protected set; }
+    public int Level { get; protected set; }
+    
+    // 생존 여부
+    public bool IsAlive => CurrentHp > 0;
+    #endregion
+    
+    #region 생성자
+
+    protected Character(string name, int maxHp, int maxMp, int attackPower, int defence, int level)
+    {
+        Name = name;
+        MaxHp = maxHp;
+        CurrentHp = maxHp;
+        MaxMp = maxMp;
+        CurrentMp = maxMp;
+        AttackPower = attackPower;
+        Defence = defence;
+        Level = level;
+    }
+    #endregion
+    
+    #region 메서드
+    
+    #endregion
+}
